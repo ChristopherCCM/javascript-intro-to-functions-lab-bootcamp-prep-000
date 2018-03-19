cba3248b2b3d2b9c777f41f17db368b2d589e6a5
Christopher <christopherccm@msn.com>
2018-03-19 14:12:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
   return "How's your mother?"
 
 }
